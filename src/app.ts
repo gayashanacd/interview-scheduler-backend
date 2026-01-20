07623f4e6e1363be55b146f61dd77d4258b7b9a5
Gayashan <gayazhanmit@yahoo.com>
2026-01-19 16:39:44 -0800

    added CORS

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import { json } from "body-parser";
+import cors from "cors";
 import userRoutes from "./routes/user.routes";
 import authRoutes from "./routes/auth.routes";
 import pingRoute from "./routes/ping.routes"
@@ -7,12 +8,20 @@ import { errorMiddleware } from "./middlewares/error.middleware";
 
 const app = express();
 
+// Cors
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
+
 // Middleware
 app.use(json());
 
 // Routes
-app.use("/api/users", userRoutes);
 app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
 
 //Basic Server Health Check health check
 app.use("/api/ping", pingRoute);
